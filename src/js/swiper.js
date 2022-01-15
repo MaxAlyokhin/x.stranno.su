a8f1a123f2a49fe14472541961f87805d7870dc4
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2022-01-15 18:23:40 +0300

    Update swiper init

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,38 +1,41 @@
 export default function swiperInit() {
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    slidesPerView: 1.3,
-    centeredSlides: true,
-    spaceBetween: 100,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 0,
+  document.querySelectorAll('.swiper').forEach((swiperElement) => {
+    const swiper = new Swiper(swiperElement, {
+      // const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+      slidesPerView: 1.3,
+      centeredSlides: true,
+      spaceBetween: 100,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
       },
-    },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+      },
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-      // hideOnClick: true,
-    },
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        // hideOnClick: true,
+      },
 
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    })
   })
 }
